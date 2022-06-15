82599e7aedb28424040ee37b14f2bb8e0fdea101
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2022-06-15 12:09:00 +0200

    Create main.dart

---
+++ day_02/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stack Widget',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Positioned(
+          height: height,
+          child: Image.asset('assets/images/background.jpg'),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            height: height / 2,
+            width: width,
+            color: Colors.black38,
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.black87,
+        )
+      ],
+    );
+  }
+}
